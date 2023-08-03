057a98a6cddea249eda52329f7449bfe626b9556
Jinson25 <am987406@gmail.com>
2023-08-03 15:12:34 -0500

    home pages

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminRoutingModule } from './components/admin/admin-routing.module';
 import { ClientRoutingModule } from './components/client/client-routing.module';
-import { ClientComponent } from './components/client/client.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
 
 const routes: Routes = [
+  {path: 'auth/login', component: LoginComponent},
+  {path: 'auth/register', component: RegisterComponent}
 ];
 
 @NgModule({
